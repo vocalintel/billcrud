e62b3301acde3e166a8dcb290aceeda70c5db612
keeshabillboard <shakirah.adeniji@iem.thm.de>
2025-05-26 22:38:39 +0200

    third commit

--- xboards/settings.py
+++ xboards/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'billboard','phonenumber_field','rest_framework',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 MIDDLEWARE = [
